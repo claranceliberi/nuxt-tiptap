4086412d2e35a139b3e7fdcb634aac9bffdaad42
claranceliberi <liberintwari@gmail.com>
2021-11-03 13:09:24 +0200

    tiptap

--- app.vue
+++ app.vue
@@ -4,9 +4,11 @@
     <div class="bg-blue-300">
       <p> name it</p>
     </div>
+    <Editor />
   </div>
 </template>
 
 <script lang="ts" setup>
 import '~/assets/styles/tailwind.scss'
+import Editor from './components/organisms/Editor.vue';
 </script>
